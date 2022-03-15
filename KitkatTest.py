d969b9f1086c728fbd222d23590087085984830e
Lordzeh <89596345+Lordzeh@users.noreply.github.com>
2022-03-15 14:44:43 +0100

    Add the last algo

--- KitkatTest.py
+++ KitkatTest.py
@@ -1,10 +1,11 @@
+from encodings import utf_8
 import hashlib
 
 # Vérifier et utiliser le type de hash
 typeOfHash = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512", "blake2b", "blake2s", "sha3_224", "sha3_256", "sha3_384", "sha3_512", "shake_1", "shake_2"]
 doneType = True
 numberOfType = 0
-numberOfTypeOfHash = 0
+foundCount = 0
 
 # Demande du type de hash
 while doneType:
@@ -12,7 +13,7 @@ while doneType:
     
     number = int(input("Combien de hash voulez vous : "))
     print("md5, sha1, sha224, sha256, sha384, sha512, blake2b, blake2s, sha3_224, sha3_256, sha3_384, sha3_512, shake_1, shake_2")
-    type = input("Quel type de hash voulez vous (si vous mettez plusieurs hash, séparez les avec des espaces): ")
+    type = input("Quel type de hash voulez vous (si vous mettez plusieurs hash, séparez les avec des espaces) : ")
     # Faire une list de chaque hash
     allType = type.split(" ")
     
@@ -20,48 +21,55 @@ while doneType:
         # Vérifier si les hash sont correct
         while number != numberOfType:
             for j in allType:
+                foundTheHash = False
                 numberOfType += 1
                 for i in typeOfHash:
-                    if j != i:
-                        theTypeIsFalse = True
-                    elif j == i:
+                    if j == i:
+                        foundTheHash = True
+                        if foundTheHash:
+                            foundCount += 1
                         break
     
-    if theTypeIsFalse == False:
+    if number == foundCount:
         doneType = False
     if doneType:
         print("\nCommande incorrect\n")
 
 # Demande de la chaîne de caractere
-#message = bytes(input("Veuillez saisir la chaîne de caractère que vous voulez chiffrer : "), encoding="utf_8")
-#
-#if type == typeOfHash[0]:
-#    hash = hashlib.new(typeOfHash[0], message)
-#elif type == typeOfHash[1]:
-#    hash = hashlib.new(typeOfHash[1], message)
-#elif type == typeOfHash[2]:
-#    hash = hashlib.new(typeOfHash[2], message)
-#elif type == typeOfHash[3]:
-#    hash = hashlib.new(typeOfHash[3], message)
-#elif type == typeOfHash[4]:
-#    hash = hashlib.new(typeOfHash[4], message)
-#elif type == typeOfHash[5]:
-#    hash = hashlib.new(typeOfHash[5], message)
-#elif type == typeOfHash[6]:
-#    hash = hashlib.new(typeOfHash[6], message)
-#elif type == typeOfHash[7]:
-#    hash = hashlib.new(typeOfHash[7], message)
-#elif type == typeOfHash[8]:
-#    hash = hashlib.new(typeOfHash[8], message)
-#elif type == typeOfHash[9]:
-#    hash = hashlib.new(typeOfHash[9], message)
-#elif type == typeOfHash[10]:
-#    hash = hashlib.new(typeOfHash[10], message)
-#elif type == typeOfHash[11]:
-#    hash = hashlib.new(typeOfHash[11], message)
-#elif type == typeOfHash[12]:
-#    hash = hashlib.new(typeOfHash[12], message)
-#elif type == typeOfHash[13]:
-#    hash = hashlib.new(typeOfHash[13], message)
-#
-#print(hash.hexdigest())
+message = bytes(input("Veuillez saisir la chaîne de caractère que vous voulez chiffrer : "), encoding="utf_8")
+
+
+for j in allType:
+    print(j)
+    if j == typeOfHash[0]:
+        hash = hashlib.new(typeOfHash[0], message)
+    elif j == typeOfHash[1]:
+        hash = hashlib.new(typeOfHash[1], message)
+    elif j == typeOfHash[2]:
+        hash = hashlib.new(typeOfHash[2], message)
+    elif j == typeOfHash[3]:
+        hash = hashlib.new(typeOfHash[3], message)
+    elif j == typeOfHash[4]:
+        hash = hashlib.new(typeOfHash[4], message)
+    elif j == typeOfHash[5]:
+        hash = hashlib.new(typeOfHash[5], message)
+    elif j == typeOfHash[6]:
+        hash = hashlib.new(typeOfHash[6], message)
+    elif j == typeOfHash[7]:
+        hash = hashlib.new(typeOfHash[7], message)
+    elif j == typeOfHash[8]:
+        hash = hashlib.new(typeOfHash[8], message)
+    elif j == typeOfHash[9]:
+        hash = hashlib.new(typeOfHash[9], message)
+    elif j == typeOfHash[10]:
+        hash = hashlib.new(typeOfHash[10], message)
+    elif j == typeOfHash[11]:
+        hash = hashlib.new(typeOfHash[11], message)
+    elif j == typeOfHash[12]:
+        hash = hashlib.new(typeOfHash[12], message)
+    elif j == typeOfHash[13]:
+        hash = hashlib.new(typeOfHash[13], message)
+    
+    message = bytes(hash.hexdigest(), encoding="utf_8")
+
+print(hash.hexdigest())
