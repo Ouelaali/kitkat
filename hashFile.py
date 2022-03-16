eac77789fe77d31eb1c82d228a5a5b470e1c30a7
watchsz <96117061+watchsz@users.noreply.github.com>
2022-03-16 16:11:44 +0100

    Ajout fonction de multiples hash sur le hsashage de fichier

--- hashFile.py
+++ hashFile.py
@@ -2,23 +2,23 @@ import sys
 import hashlib
 
 typeOfHash = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512", "blake2b", "blake2s", "sha3_224", "sha3_256", "sha3_384", "sha3_512", "shake_1", "shake_2"]
-md5 = hashlib.md5()
-sha1 = hashlib.sha1()
 
 #Fonction qui permet de hasher dans tous les algorithmes que l'utilisateur a entré
-def finalHash(selectedHashes, data):
-    for i in selectedHashes:
+def finalHash(allType, data):
+    for i in allType:
         if i in typeOfHash:
             print(i + " :  {0}".format(hashlib.new(i, data).hexdigest()))
+            data = bytes(hashlib.new(i, data).hexdigest(), encoding="utf_8")
+            
 
 # 'rb' permet de lire le fichier en binaire 
 # avec f.read() nous pouvons lire et permettre de l'utiliser
 with open(sys.argv[1], 'rb') as f:
-    
-    selectedHashes = input('Avec quel algorithme de hash parmis ceux du dessous vouslez-vous hasher le fichier ?')
+    print(*typeOfHash)
+    selectedHashes = input('Choose the type on hash to use on the file (if you put several hashes, separate them with spaces): ')
+    allType = selectedHashes.split(" ")
     while True:
         data = f.read(65536)
-        # print(data)
         if not data:
             break
-        finalHash(selectedHashes, data)
+        finalHash(allType, data)
