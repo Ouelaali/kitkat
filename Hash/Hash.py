84c7d6b942b36f757f855253bd1ece7bb1263bb5
Lordzeh <89596345+Lordzeh@users.noreply.github.com>
2022-03-17 14:22:53 +0100

    add def for the type of hash

--- Hash/Hash.py
+++ Hash/Hash.py
@@ -7,3 +7,38 @@ class Hash:
             message = bytes(hash.hexdigest(), encoding="utf_8")
 
         print(hash.hexdigest())
+    
+    def typeOfHash():
+        # Ask the type of hash
+        typeOfHash = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512", "blake2b", "blake2s", "sha3_224", "sha3_256", "sha3_384", "sha3_512", "shake_1", "shake_2"]
+        # theTypeIsFalse = False
+        doneType = True
+        numberOfType = 0
+        foundCount = 0
+        while doneType:
+            
+            
+            number = int(input("How many hashes do you want : "))
+            print("md5, sha1, sha224, sha256, sha384, sha512, blake2b, blake2s, sha3_224, sha3_256, sha3_384, sha3_512, shake_1, shake_2")
+            type = input("What type of hash do you want (if you put several hashes, separate them with spaces) : ")
+            # Do a list of hash
+            allType = type.split(" ")
+            
+            if number == len(allType):
+                # Verify the hash
+                while number != numberOfType:
+                    for j in allType:
+                        foundTheHash = False
+                        numberOfType += 1
+                        for i in typeOfHash:
+                            if j == i:
+                                foundTheHash = True
+                                if foundTheHash:
+                                    foundCount += 1
+                                break
+            
+            if number == foundCount:
+                doneType = False
+            if doneType:
+                print("\nIncorrect order\n")
+        return allType
--- Hash/KitkatTest.py
+++ Hash/KitkatTest.py
@@ -1,37 +1,6 @@
 from Hash import Hash
 
-typeOfHash = ["md5", "sha1", "sha224", "sha256", "sha384", "sha512", "blake2b", "blake2s", "sha3_224", "sha3_256", "sha3_384", "sha3_512", "shake_1", "shake_2"]
-doneType = True
-numberOfType = 0
-foundCount = 0
-
-# Ask the type of hash
-while doneType:
-    theTypeIsFalse = False
-    
-    number = int(input("How many hashes do you want : "))
-    print("md5, sha1, sha224, sha256, sha384, sha512, blake2b, blake2s, sha3_224, sha3_256, sha3_384, sha3_512, shake_1, shake_2")
-    type = input("What type of hash do you want (if you put several hashes, separate them with spaces) : ")
-    # Do a list of hash
-    allType = type.split(" ")
-    
-    if number == len(allType):
-        # Verify the hash
-        while number != numberOfType:
-            for j in allType:
-                foundTheHash = False
-                numberOfType += 1
-                for i in typeOfHash:
-                    if j == i:
-                        foundTheHash = True
-                        if foundTheHash:
-                            foundCount += 1
-                        break
-    
-    if number == foundCount:
-        doneType = False
-    if doneType:
-        print("\nIncorrect order\n")
+allType = Hash.typeOfHash()
 
 # Ask the string
 message = bytes(input("Please enter the string you want to encrypt : "), encoding="utf_8")
