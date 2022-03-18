5d2eb65fd4420531a2224f7e6121f599862c1961
Lordzeh <89596345+Lordzeh@users.noreply.github.com>
2022-03-18 11:50:26 +0100

    Change help

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from hashString import hashTheString
 from hashFile import hashTheFile
+from help import helpHash
 
 menu_options = {
     1: 'Hash with file',
     2: 'Hash with string',
     3: 'Help',
     4: 'Exit',
-}
-
-menuu_options =  {
-    1: 'Help Hash',
-    2: 'Help',
-    3: 'return'
 }
 
 def print_menu():
@@ -35,31 +30,12 @@ def mainMenu():
         elif option == 2:
             hashTheString()
         elif option == 3:
-            helpMenu()
+            helpHash()
         elif option == 4:
             print('exit')
             exit()
         else: 
              print('\nPlease enter a number between 1 and 4.\n')
-
-def helpMenu():
-    while(True):
-        print_menuu()
-        option = ''
-        try:
-            option = int(input('Enter your choice :  '))
-        except:
-            print('\nWrong input. Please enter a number ...\n')
-        if option == 1:
-            print('Handle option \'Option 1\'')
-        elif option == 2:
-            print('Handle option \'Option 2\'')
-        elif option == 3:
-            print('return')
-            mainMenu()
-        else: 
-            print('\nInvalid option. Please enter a number between 1 and 2\n')
-
 
 if __name__ == '__main__':
     mainMenu()
