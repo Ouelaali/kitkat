35c25dcfde71ea0d2baeb83f4f2591b2531e50ab
watchsz <96117061+watchsz@users.noreply.github.com>
2022-03-18 11:12:40 +0100

    changement de main, menu qui marche

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from optionT import *
 from hashString import hashTheString
 from hashFile import hashTheFile
 
@@ -7,6 +6,12 @@ menu_options = {
     2: 'Hash with string',
     3: 'Help',
     4: 'Exit',
+}
+
+menuu_options =  {
+    1: 'Help Hash',
+    2: 'Help',
+    3: 'return'
 }
 
 def print_menu():
@@ -22,8 +27,7 @@ def option3():
     new = input("Enter your choice : ")
     
 
-
-if __name__ == '__main__':
+def mainMenu():
     while(True):
         print_menu()
         option = ''
@@ -32,13 +36,36 @@ if __name__ == '__main__':
         except:
             print('\nWrong input....')
         if option == 1:
-            option1()
+            hashTheFile()
         elif option == 2:
-            option2()
+            hashTheString()
         elif option == 3:
-            option3()
+            helpMenu()
         elif option == 4:
             print('exit')
             exit()
         else: 
              print('\nPlease enter a number between 1 and 4.\n')
+
+def helpMenu():
+    while(True):
+        print_menuu()
+        option = ''
+        try:
+            option = int(input('Enter your choice :  '))
+        except:
+            print('\nWrong input. Please enter a number ...\n')
+        if option == 1:
+            print('Handle option \'Option 1\'')
+        elif option == 2:
+            print('Handle option \'Option 2\'')
+        elif option == 3:
+            print('return')
+            mainMenu()
+            exit()
+        else: 
+            print('\nInvalid option. Please enter a number between 1 and 2\n')
+
+
+if __name__ == '__main__':
+    mainMenu()
